08b954bc9ae739bc98292b634f8c39d0efa59950
aristides1000 <aristides1000@gmail.com>
2018-08-09 17:29:48 -0400

    commit laravel 5.6 video 3 minuto 6:52
     laravel 5.6 video 3 minuto 6:52

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,34 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function (){
+    return 'Home';
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/usuarios', function () {
+    return 'Usuarios';
+});
+
+//Por ende
+//usuarios/nuevo != usuarios/[0-9]+
+//OJO [0-9]+ == \d+ OJO
+//OJO la expresión regular \w+ acepta letras y números
+
+//La otra solución es cambiar el orden
+/*
+Route::get('/usuarios/{id}', function ($id) {
+    return "mostrando detalle del usuario: {$id}";
+})->where('id', '\d+');
+
+Route::get('/usuarios/nuevo', function () {
+    return 'Crear nuevo usuario';
+});
+ */
+
+//aquí hicimos el cambio del órden de las rutas
+Route::get('/usuarios/nuevo', function () {
+    return 'Crear nuevo usuario';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return "mostrando detalle del usuario: {$id}";
 });
